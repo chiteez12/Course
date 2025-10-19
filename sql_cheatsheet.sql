fabd703d9338eff75ee3000e0f35ccd93ebec4c1
xyz <138595459+chiteez12@users.noreply.github.com>
2025-10-19 13:02:32 +0530

    Update sql_cheatsheet.sql

--- sql_cheatsheet.sql
+++ sql_cheatsheet.sql
@@ -61,3 +61,58 @@ ALTER TABLE students ADD CONSTRAINT unique_name UNIQUE(name); -- unique_name is 
 
 -- Add check constraint:
 ALTER TABLE students ADD CONSTRAINT chk_dob CHECK(dob > '2007-01-01');
+
+-- Functions: Important sql functions
+-- 1. COUNT() - counts the number of rows:
+SELECT COUNT(*) FROM students;
+
+-- 2. NOW() - returns the current date and time
+SELECT NOW();
+
+-- 3. CONCAT() - concats two strings.
+SELECT CONCAT('A','B');
+
+-- 4. LENGTH() - returns the length of a string in bytes;
+SELECT LENGTH('abc');
+
+-- 5. ROUND(num, places) - rounds off the given number to n decimal places.
+SELECT ROUND(24.67, 1);
+
+-- 6. DATEDIFF() - returns the difference between two dates (in days).
+SELECT DATEDIFF('2025-06-01', '2025-05-01');
+
+-- 7. MIN() - returns the minimum value from the given data
+SELECT MIN(marks) FROM students;
+
+-- 8. MAX() - returns the maximum value from the given data
+SELECT MAX(marks) FROM students;
+
+-- 9. SUM() - returns the total of the given data
+SELECT SUM(marks) FROM students;
+
+-- 10. AVG() - returns the average of the given data
+SELECT AVG(marks) FROM students;
+
+-- 11. IF(condition, value if True, value if False) - apply a logical condition to the given value
+SELECT marks, IF(marks > 70, 'Pass', 'Fail') FROM students;
+
+
+-- GROUP BY: summarises the data into groups.
+SELECT * FROM books GROUP BY genre;
+
+-- ORDER BY: Arrange the data in ascending or descending order
+SELECT * FROM students ORDER BY marks DESC;
+SELECT * FROM students ORDER BY marks ASC;
+
+-- Autocommit
+-- By default, sql queries change the data permanently. However, we can modify this behaviour with:
+SET autocommit = 0; -- sets autocommit to false.
+COMMIT; -- saves the changes done till now.
+
+-- Now we can perform any queries without any fear of losing the data. If we do a mistake, we can go back to the previous data using:
+ROLLBACK;
+
+-- If we want to apply the changes permanently to the database, then:
+COMMIT;
+
+-- Note: DDL commands are an exception.
